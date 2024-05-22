e095758a2c2d2cc86fae9f018b96602e1c0903c4
akaWild <nemyx84@gmail.com>
2024-05-22 23:13:21 +0300

    Added HandleResult to base activity controller to handle responses

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -1,4 +1,5 @@
-﻿using MediatR;
+﻿using Application.Core;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
@@ -9,5 +10,19 @@ namespace API.Controllers
     {
         private IMediator _mediator;
         protected IMediator Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
+
+        protected ActionResult HandleResult<T>(Result<T>? result)
+        {
+            if (result == null)
+                return NotFound();
+
+            if (result is { IsSuccess: true, Value: not null })
+                return Ok(result.Value);
+
+            if (result is { IsSuccess: true, Value: null })
+                return NotFound();
+
+            return BadRequest(result.Error);
+        }
     }
 }
