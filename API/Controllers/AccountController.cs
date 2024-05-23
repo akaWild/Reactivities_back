df9cc21591cfc0be06c68c1bad041ba6ee9eefed
akaWild <nemyx84@gmail.com>
2024-05-23 23:35:41 +0300

    Added GetCurrentUser endpoint (+some refactoring in  account controller)

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -5,10 +5,10 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
 
 namespace API.Controllers
 {
-    [AllowAnonymous]
     [ApiController]
     [Route("api/[controller]")]
     public class AccountController : ControllerBase
@@ -21,6 +21,7 @@ namespace API.Controllers
             _tokenService = tokenService;
         }
 
+        [AllowAnonymous]
         [HttpPost("login")]
         public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
         {
@@ -30,19 +31,12 @@ namespace API.Controllers
 
             bool result = await _userManager.CheckPasswordAsync(user, loginDto.Password);
             if (result)
-            {
-                return new UserDto
-                {
-                    DisplayName = user.DisplayName,
-                    Image = null,
-                    Token = _tokenService.CreateToken(user),
-                    Username = user.DisplayName,
-                };
-            }
+                return CreateUserObject(user);
 
             return Unauthorized();
         }
 
+        [AllowAnonymous]
         [HttpPost("register")]
         public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
         {
@@ -62,17 +56,29 @@ namespace API.Controllers
             IdentityResult result = await _userManager.CreateAsync(user, registerDto.Password);
 
             if (result.Succeeded)
-            {
-                return new UserDto
-                {
-                    DisplayName = user.DisplayName,
-                    Image = null,
-                    Token = _tokenService.CreateToken(user),
-                    Username = user.UserName
-                };
-            }
+                return CreateUserObject(user);
+
 
             return BadRequest(result.Errors);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<UserDto>> GetCurrentUser()
+        {
+            AppUser? user = await _userManager.FindByEmailAsync(User.FindFirstValue(ClaimTypes.Email));
+
+            return CreateUserObject(user);
+        }
+
+        private UserDto CreateUserObject(AppUser? user)
+        {
+            return new UserDto
+            {
+                DisplayName = user?.DisplayName,
+                Image = null,
+                Token = _tokenService.CreateToken(user),
+                Username = user.UserName
+            };
         }
     }
 }
