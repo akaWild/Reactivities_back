f447b637e32dc7296053fb6af602755f02909f5e
akaWild <nemyx84@gmail.com>
2024-05-25 00:52:57 +0300

    Fixed bug: wrong automapper profile config

--- Application/Core/MappingProfiles.cs
+++ Application/Core/MappingProfiles.cs
@@ -10,11 +10,13 @@ namespace Application.Core
         {
             CreateMap<Activity, Activity>();
 
-            CreateMap<Activity, ActivityDto>().ForMember(d => d.HostUsername, o => o.MapFrom(s => s.Attendees.FirstOrDefault(x => x.IsHost).AppUser.UserName));
+            CreateMap<Activity, ActivityDto>()
+                .ForMember(d => d.HostUsername, o => o.MapFrom(s => s.Attendees.FirstOrDefault(x => x.IsHost).AppUser.UserName));
 
-            CreateMap<ActivityAttendee, Profiles.Profile>().ForMember(d => d.DisplayName, o => o.MapFrom(s => s.AppUser.DisplayName));
-            CreateMap<ActivityAttendee, Profiles.Profile>().ForMember(d => d.Username, o => o.MapFrom(s => s.AppUser.UserName));
-            CreateMap<ActivityAttendee, Profiles.Profile>().ForMember(d => d.Bio, o => o.MapFrom(s => s.AppUser.Bio));
+            CreateMap<ActivityAttendee, Profiles.Profile>()
+                .ForMember(d => d.DisplayName, o => o.MapFrom(s => s.AppUser.DisplayName))
+                .ForMember(d => d.Username, o => o.MapFrom(s => s.AppUser.UserName))
+                .ForMember(d => d.Bio, o => o.MapFrom(s => s.AppUser.Bio));
         }
     }
 }
