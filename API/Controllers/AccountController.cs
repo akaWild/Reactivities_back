e0087c6d1c7d48cded1852d6f300af7c112eb665
akaWild <nemyx84@gmail.com>
2024-05-24 18:50:41 +0300

    Made duplicate email/username as validation problem instead of bad request

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -41,10 +41,20 @@ namespace API.Controllers
         public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
         {
             if (await _userManager.Users.AnyAsync(x => x.UserName == registerDto.Username))
-                return BadRequest("Username is already taken");
+            {
+                ModelState.AddModelError("username", "Username is already taken");
+
+                return ValidationProblem(ModelState);
+            }
 
             if (await _userManager.Users.AnyAsync(x => x.Email == registerDto.Email))
-                return BadRequest("Email is already taken");
+            {
+                {
+                    ModelState.AddModelError("email", "Email is already taken");
+
+                    return ValidationProblem(ModelState);
+                }
+            }
 
             AppUser? user = new AppUser
             {
