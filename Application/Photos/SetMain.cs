23e5462cca37f4224f3b2b1e7e53f5369790ea4b
akaWild <nemyx84@gmail.com>
2024-05-26 15:51:13 +0300

    Added set main photo handler

---
+++ Application/Photos/SetMain.cs
@@ -0,0 +1,52 @@
+﻿using Application.Core;
+using Application.Interfaces;
+using Domain;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace Application.Photos
+{
+    public class SetMain
+    {
+        public class Command : IRequest<Result<Unit>>
+        {
+            public string Id { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Command, Result<Unit>>
+        {
+            private readonly DataContext _context;
+            private readonly IUserAccessor _userAccessor;
+            public Handler(DataContext context, IUserAccessor userAccessor)
+            {
+                _context = context;
+                _userAccessor = userAccessor;
+            }
+
+            public async Task<Result<Unit>> Handle(Command request, CancellationToken cancellationToken)
+            {
+                AppUser? user = await _context.Users.Include(p => p.Photos).FirstOrDefaultAsync(x => x.UserName == _userAccessor.GetUsername(), cancellationToken);
+
+                if (user == null)
+                    return null;
+
+                Photo? photo = user.Photos.FirstOrDefault(x => x.Id == request.Id);
+                if (photo == null)
+                    return null;
+
+                Photo? currentMain = user.Photos.FirstOrDefault(x => x.IsMain);
+                if (currentMain != null)
+                    currentMain.IsMain = false;
+
+                photo.IsMain = true;
+
+                bool success = await _context.SaveChangesAsync(cancellationToken) > 0;
+                if (success)
+                    return Result<Unit>.Success(Unit.Value);
+
+                return Result<Unit>.Failure("Problem setting main photo");
+            }
+        }
+    }
+}
